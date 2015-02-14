886ff7a2772a0d083580c5f38445c1ca0d405a16
Samoilenko Yuri <kinnalru@gmail.com>
2015-02-14 16:57:53 +0300

    work in progress

--- storage.cpp
+++ storage.cpp
@@ -12,12 +12,13 @@
 #include "tools.h"
 
 namespace fs = boost::filesystem;
+typedef boost::property_tree::ptree config_t;
 
 typedef boost::filter_iterator<std::function<bool(const fs::directory_entry&)>, fs::directory_iterator> files_iterator;
 
 struct fs_storage_t : storage_t {
-    fs_storage_t(const boost::property_tree::ptree& c, const std::string& pin)
-        : storage_t(c) 
+    fs_storage_t(const config_t& c, const std::string& pin, std::shared_ptr<storage_t> s)
+        : storage_t(c, s) 
     {
         path = config_.get<std::string>("path");
         st_logf("Path : %s\n", path.c_str());
@@ -89,42 +90,74 @@ struct mount_t {
     std::string umount;
 };
 
-typedef std::shared_ptr<mount_t> mount_p;
-
-struct mountable_storage_t : fs_storage_t {
-    mountable_storage_t(const boost::property_tree::ptree& c, const std::string& pin)
-        : fs_storage_t(c, pin) 
+struct shell_storage_t : fs_storage_t {
+    shell_storage_t(const config_t& c, const std::string& pin, std::shared_ptr<storage_t> s = std::shared_ptr<storage_t>())
+        : fs_storage_t(c, pin, s)
     {
-        BOOST_FOREACH(auto p, c) {
-            if (p.first.find("mount") == 0) {
-                std::string id = p.first.substr(p.first.find("_") + 1);
-                std::string mount = p.second.get_value<std::string>();
-                std::string umount = c.get<std::string>("umount_" + id);
+        const std::string mount = c.get<std::string>("mount");
+        const std::string umount = c.get<std::string>("umount");
         
-                st_logf("Mount: %s, Umount: %s\n", mount.c_str(), umount.c_str());
-                mounts.push_back(mount_p(new mount_t(mount, umount, pin)));
+        st_logf("Mount: %s, Umount: %s\n", mount.c_str(), umount.c_str());
+        m_.reset(new mount_t(mount, umount, pin));
+    }
+    
+    std::shared_ptr<mount_t> m_;
+};
+
+struct cryptfs_storage_t : storage_t {
+    cryptfs_storage_t(const config_t& c, const std::string& pin, std::shared_ptr<storage_t> s)
+        : storage_t(c,s)
+    {
+        decrypt_ = "openssl enc -d -base64 " + c.get<std::string>("cipher") + " -k '" + pin + "'";
+    }
+    
+    virtual ~cryptfs_storage_t() {
+    }
+    
+    virtual std::list<item_t> items() {
+        std::list<item_t> result;
+        for(auto item: prev->items()) {
+            result.push_back(decrypt(item));
+        }
+        
+        return result;
+    }
+    
+    virtual item_t read(const std::string& fn) {
+        return decrypt(prev->read(fn));
+    }
+    
+    
+    
+    item_t decrypt(const item_t& item) const {
+        return item_t {
+            item.fullname,
+            item.filename,
+            piped(decrypt_, item.data)
+        };
+    }
+    
+    std::string decrypt_;
+    
+};
+
+
+std::shared_ptr<storage_t> storage_t::create(const config_t& config, const std::string& pin)
+{
+    std::shared_ptr<storage_t> storage;
+    BOOST_FOREACH(auto p, config) {
+        if (p.second.size() > 0) {
+            if (p.second.get<std::string>("driver") == "shell") {
+                storage.reset(new shell_storage_t(p.second, pin, storage));
+            }
+            else if (p.second.get<std::string>("driver") == "crypt") {
+                storage.reset(new cryptfs_storage_t(p.second, pin, storage));
             }
         }
     }
     
-    virtual ~mountable_storage_t() {
-        while (!mounts.empty()) {
-            mounts.pop_back();
-        }
-    }
-    
-    std::list<mount_p> mounts;
-};
+    std::cerr << "data:" << storage->read("crypt").data.data() << std::endl;
+    exit(-1);
 
-
-std::shared_ptr<storage_t> storage_t::create(const boost::property_tree::ptree& config, const std::string& pin)
-{
-    auto driver = config.get<std::string>("driver");
-    
-    if (driver == "mountfs") {
-        return std::shared_ptr<storage_t>(new mountable_storage_t(config.get_child("mountfs"), pin));    
-    } 
-    else {
-        return std::shared_ptr<storage_t>(new fs_storage_t(config, pin));
-    }
+    return storage;
 }
--- storage.h
+++ storage.h
@@ -18,7 +18,7 @@ struct item_t {
     
     const std::string fullname; //FIXME HACK remove
     const std::string filename;
-    const std::vector<char> data;
+    /*const*/ std::vector<char> data;
 };
 
 struct storage_t {
@@ -29,14 +29,15 @@ struct storage_t {
     
     virtual std::list<item_t> items() = 0;
     virtual item_t read(const std::string& fn) = 0;
-    
 
 protected:
-    storage_t(const boost::property_tree::ptree& c) : config_(c){};
+    storage_t(const boost::property_tree::ptree& c, std::shared_ptr<storage_t> s = std::shared_ptr<storage_t>()) : prev(s), config_(c){};
     storage_t(const storage_t& other) = delete;
     storage_t& operator=(const storage_t& other) = delete;
-    
+
+    std::shared_ptr<storage_t> prev;
     boost::property_tree::ptree config_;
+    std::string path_;
 };
 
 
